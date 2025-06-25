588a93fbd391eb7b17a7eabee49d297a66ab1f4d
aknkya <akn_kya@hotmail.com>
2025-06-25 19:49:45 +0300

    new Cors origin added for server

--- backend/src/main/java/com/restaurant/backend/config/WebConfig.java
+++ backend/src/main/java/com/restaurant/backend/config/WebConfig.java
@@ -13,9 +13,13 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "http://172.238.101.163:3000"
+                        )
                         .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
