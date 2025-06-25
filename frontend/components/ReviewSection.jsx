4578383493827d36a6435ebab4be71870704bba1
Emre Alma <emrealma@hotmail.com>
2025-06-26 01:15:10 +0200

    Improve layout or wording in review section

--- frontend/components/ReviewSection.jsx
+++ frontend/components/ReviewSection.jsx
@@ -5,10 +5,10 @@ const ReviewSection = () => {
   return (
     <section id="review" className="py-16 px-8">
       <h3 className="text-center text-2xl text-jellyBeanBlue">
-        Customer Reviews
+        Kundenbewertungen
       </h3>
       <h1 className="text-center text-3xl text-sunset uppercase mb-8">
-        What Our Guests Say
+        Das sagen unsere Gäste
       </h1>
       <Swiper
         spaceBetween={20}
@@ -31,38 +31,12 @@ const ReviewSection = () => {
             <i className="fas fa-quote-right text-4xl text-gray-300 absolute top-4 right-4"></i>
             <div className="flex items-center space-x-4 mb-4">
               <img
-                src="/images/pic-1.png"
+                src="/images/pic-2.png"
                 alt="Willis Mack"
                 className="w-16 h-16 rounded-full"
               />
               <div>
-                <h3 className="text-xl text-sunset font-bold">Willis Mack</h3>
-                <div className="flex space-x-1">
-                  <i className="fas fa-star text-jellyBeanBlue"></i>
-                  <i className="fas fa-star text-jellyBeanBlue"></i>
-                  <i className="fas fa-star text-jellyBeanBlue"></i>
-                  <i className="fas fa-star text-jellyBeanBlue"></i>
-                  <i className="fas fa-star-half-alt text-jellyBeanBlue"></i>
-                </div>
-              </div>
-            </div>
-            <p className="text-lightColor">
-              The food was incredible, and the atmosphere was warm and
-              welcoming. I loved every bite!
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="bg-white p-6 rounded shadow relative">
-            <i className="fas fa-quote-right text-4xl text-gray-300 absolute top-4 right-4"></i>
-            <div className="flex items-center space-x-4 mb-4">
-              <img
-                src="/images/pic-2.png"
-                alt="Jane Kim"
-                className="w-16 h-16 rounded-full"
-              />
-              <div>
-                <h3 className="text-xl text-sunset font-bold">Jane Kim</h3>
+                <h3 className="text-xl text-sunset font-bold">Gabi Shär</h3>
                 <div className="flex space-x-1">
                   <i className="fas fa-star text-jellyBeanBlue"></i>
                   <i className="fas fa-star text-jellyBeanBlue"></i>
@@ -73,8 +47,8 @@ const ReviewSection = () => {
               </div>
             </div>
             <p className="text-lightColor">
-              A hidden gem! The flavors were on point, and the staff made sure
-              we had an amazing time.
+              Sehr, sehr freundliche Bedienung, Essen ausgezeichnet, es wurde alles frisch zubereitet. Wir kommen gerne wieder.
+Auch beim zweiten mal überzeugte die Küche. Wenn es nicht soweit entfernt von uns wäre, wären wir wohl gerne noch öfter dort zum Essen :)
             </p>
           </div>
         </SwiperSlide>
@@ -88,19 +62,43 @@ const ReviewSection = () => {
                 className="w-16 h-16 rounded-full"
               />
               <div>
-                <h3 className="text-xl text-sunset font-bold">Ronald Doe</h3>
+                <h3 className="text-xl text-sunset font-bold">Kevin Patrick Zollinger</h3>
                 <div className="flex space-x-1">
                   <i className="fas fa-star text-jellyBeanBlue"></i>
                   <i className="fas fa-star text-jellyBeanBlue"></i>
                   <i className="fas fa-star text-jellyBeanBlue"></i>
                   <i className="fas fa-star text-jellyBeanBlue"></i>
-                  <i className="fas fa-star-half-alt text-jellyBeanBlue"></i>
+                  <i className="fas fa-star text-jellyBeanBlue"></i>
                 </div>
               </div>
             </div>
             <p className="text-lightColor">
-              A must-visit restaurant! The quality of the food and service truly
-              exceeded my expectations.
+              Bester Imbiss in der Region! Immer empfehlenswert und super Qualität!
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="bg-white p-6 rounded shadow relative">
+            <i className="fas fa-quote-right text-4xl text-gray-300 absolute top-4 right-4"></i>
+            <div className="flex items-center space-x-4 mb-4">
+              <img
+                src="/images/pic-2.png"
+                alt="Jane Kim"
+                className="w-16 h-16 rounded-full"
+              />
+              <div>
+                <h3 className="text-xl text-sunset font-bold">Nadya Wolflisberg</h3>
+                <div className="flex space-x-1">
+                  <i className="fas fa-star text-jellyBeanBlue"></i>
+                  <i className="fas fa-star text-jellyBeanBlue"></i>
+                  <i className="fas fa-star text-jellyBeanBlue"></i>
+                  <i className="fas fa-star text-jellyBeanBlue"></i>
+                  <i className="fas fa-star text-jellyBeanBlue"></i>
+                </div>
+              </div>
+            </div>
+            <p className="text-lightColor">
+              Sehr zuvorkommend. Trotz grösserer Bestellung für eine Gesellschaft, pünktliche und richtige Lieferung. Preise passen. Essen frisch und gut. Empfehlenswert.
             </p>
           </div>
         </SwiperSlide>
